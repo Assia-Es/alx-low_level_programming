6a475fe826617770354aa989d73601fdc4c65af9
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-04-20 01:22:02 -0700

    hello, world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main - A C program that prints a line to the standard error
  *
  * Return: Always 1 (Success)
  */
 int main(void){
-        fwrite(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+        fput(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 return (1);
 }
