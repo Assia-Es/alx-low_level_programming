4a5fe90dc3572aeb9f554b46ac627b7f6fb980bf
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-04-19 23:51:41 -0700

    hello, world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+        printf(" char:%d\n",sizeof(char));
+	printf(" int :%d\n",sizeof(int));
+	printf(" long int :%d\n",sizeof(long int));
+	printf(" float :%d\n",sizeof(float));
+return(0);
+}
