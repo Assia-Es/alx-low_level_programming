86321c6f078c315eb61eefa48f8a930e93884cf1
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-04-20 00:24:12 -0700

    hello, w

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
- * main -Entry poit
- * return: always 0 (success)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main(){
+int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
