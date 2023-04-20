8557304d6dc2538171b9a71907c6d32b67cd6e42
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-04-20 00:37:21 -0700

    hel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
-int main(){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
         printf(" char:%d\n",sizeof(char));
+	printf(" char long:%d\n",sizeof(char long));
 	printf(" int :%d\n",sizeof(int));
 	printf(" long int :%d\n",sizeof(long int));
+	printf(" long long int:%d\n",sizeof(long long int));
 	printf(" float :%d\n",sizeof(float));
-return(0);
+return (0);
 }
