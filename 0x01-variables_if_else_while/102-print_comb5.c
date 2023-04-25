1f9373144d267ecbb79749fc9a6a954788b5cf94
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-04-24 19:37:45 -0700

    var

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: alphabet in lowercase
+ * Return: always 0
+ */
+int main(void)
+{
+int n;
+int m;
+int p;
+int t;
+for (n = 48 ; n <= 57 ; n++)
+{
+
+for (m = 48  ; m <= 56 ; m++)
+{
+for (p = 48  ; p <= 57 ; p++)
+{
+for (t = p + 1  ; t <= 57 ; t++)
+{
+putchar(n);
+putchar(m);
+putchar(p);
+if (n == 57 && m == 56 && p == 57 && t==57)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
