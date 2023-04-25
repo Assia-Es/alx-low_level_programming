4a874a2eeefb2be628300896d4a5266ce29df657
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-04-24 17:36:05 -0700

    varia

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,13 +9,9 @@ int main(void)
 {
 int n;
 
-for (n = 97 ; n <= 122 ; n++)
+for (n = 0 ; n <= 9 ; n++)
 {
-if (n == 101 || n == 113)
-{
-continue;
-}
-putchar(n);
+putchar(n + 48);
 }
 putchar('\n');
 return (0);
