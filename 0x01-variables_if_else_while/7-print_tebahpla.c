b783e4b56bfb3d58d7ff0022eeac19dc5fbe48c5
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-04-24 17:47:45 -0700

    vari

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: alphabet in lowercase
+ * Return: always 0
+ */
+int main(void)
+{
+int n;
+
+for (n = 122 ; n <= 97 ; n--)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
