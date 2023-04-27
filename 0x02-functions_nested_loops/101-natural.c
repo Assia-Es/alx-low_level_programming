7153b2c90fc9a96f90d82d516beafb76963bd593
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-04-26 18:44:05 -0700

    function

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+/*
+ * File: 101-natural.c
+ * Auth: assia
+ */
+#include <stdio.h>
+/**
+ * main : ana ecrire
+ *
+ * Return: Alwayes 0.
+ */
+int main(void)
+{
+int i, sum = 0;
+for (i = 0; i < 1024 ; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+{
+	sum += i;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
