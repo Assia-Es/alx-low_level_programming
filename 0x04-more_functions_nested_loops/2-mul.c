3bc351947e734d817ecc82b35007d5e0dc364b4d
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-04-27 19:49:04 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul -  for uppercase character..
+ *
+ * @a: number
+ * @b: number
+ * Return: mult.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
