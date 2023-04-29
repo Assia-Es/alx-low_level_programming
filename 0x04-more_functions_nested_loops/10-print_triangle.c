8d89e2b72b1c55f9e22eb581077146e17f06f889
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-04-28 19:07:36 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - for uppercase character..
+ *
+ * @size: Number.
+ */
+
+void print_triangle(int size)
+{
+int i, j;
+if (size > 0)
+{
+for (i = 1; i <= size; i++)
+{
+for (j = i; j < size; j++)
+{
+_putchar(' ');
+}
+for (j = 1; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
