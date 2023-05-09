5e58f8e439caa4033a6d9292355a43f59a1829a6
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-05-09 06:25:59 -0700

    string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - writes the character c to stdout
+ * @s: The character to print
+ * @to: The character to print
+ * Return: s
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
