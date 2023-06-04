f4ea8c2d82b27836125a51fa87523b276779561d
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-06-04 02:49:13 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int check(char *s, int len, int i);
+int check_pal(char *s, int len, int i);
 int _strlen_recursion(char *s);
 /**
  * is_palindrome - check the code
@@ -13,7 +13,7 @@ int is_palindrome(char *s)
 
 if (*s == 0)
 return (1);
-return (check(s, 0, _strlen_recursion(s)));
+return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
  * _strlen_recursion - check the code
@@ -29,18 +29,18 @@ return (0);
 return (1 + _strlen_recursion(s + 1));
 }
 /**
- * check - check the code
+ * check_pal - check the code
  * @s: parametre
  * @len: par
  * @i: parametre
  * Return: fact
  */
-int check(char *s, int len, int i)
+int check_pal(char *s, int len, int i)
 {
 
 if (*(s + 1) != *(s + len - 1))
 return (0);
 if (i >= len)
 return (1);
-return (check(s, i + 1, len - 1));
+return (check_pal(s, i + 1, len - 1));
 }
