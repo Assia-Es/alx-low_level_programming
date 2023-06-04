dfc8bcd529c73db6033447c59f5fbc6e81c75fe0
Assia-Es <es-salhi.ensa@uhp.ac.ma>
2023-06-04 06:46:34 -0700

    static

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,11 +1,28 @@
-#include <time.h>
+#include "main.h"
 
 /**
- * main - check the code
+ * _atoi - check the code
  *
- * Return: Always 0.
+ * @s:para
+ *
+ * Returninterger
  */
-int main(void)
+int _atoi(char *s)
 {
-int nb;
+int sign = 1, i = 0;
+unsigned int res = 0;
+
+while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+{
+if (s[i] == '-')
+sign *= -1;
+i++;
+}
+while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+{
+res = (res * 10) + (s[i] - '0');
+i++;
+}
+res *= sign;
+return (res);
 }
